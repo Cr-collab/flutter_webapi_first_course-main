9ef1b3c0b30a37083f2c4f0e2ffcf767a75e2aa3
Cr-collab <cristiano.costa@cyan-agro.com>
2023-01-12 19:34:17 -0300

    finalizando funcionalidades da tela de login

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_interceptor/http/http.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   // TODO: Modularizar o endpoint
@@ -29,6 +30,7 @@ class AuthService {
       }
       throw HttpException(response.body);
     }
+    saveUserInfos(response.body);
 
     return true;
   }
@@ -41,6 +43,28 @@ class AuthService {
         'password': password,
       },
     );
+
+    if (response.statusCode != 201) {
+      throw HttpException(response.body);
+    }
+  }
+
+  saveUserInfos(String body) async {
+    print('aquii');
+    Map<String, dynamic> map = json.decode(body);
+
+    String token = map["accessToken"];
+    String email = map["user"]["email"];
+    int id = map["user"]["id"];
+    print("$token\n$email\n$id");
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("acessToken", token);
+    prefs.setString("email", email);
+    prefs.setInt("id", id);
+
+    String? tokenSalvo = prefs.getString("acessToken");
+    print(tokenSalvo);
   }
 }
 
